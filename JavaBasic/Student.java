bd22d505bc9ffa35aa4ee1a9b5280cddc4c5ee28
kwakmoonseok <kms34510@naver.com>
2020-12-22 09:17:51 +0900

    Day2 / 참조 자료형
    다른 클래스로 만든 인스턴스를 변수로 사용할 수 있음.

--- JavaBasic/Student.java
+++ JavaBasic/Student.java
@@ -1,7 +1,8 @@
 package JavaBasic;
 
-class boo {
-	
+class Subject {
+	String subjectName;
+	int score;
 }
 /* 학생에 대한 정보를 보여주는 클래스*/
 
@@ -11,28 +12,37 @@ public class Student { //public class 이름과 클래스 파일 이름은 같아야함.
 	public int studentID;
 	//private - 다른 클래스에서 직접 접근 불가능함
 	public String studentName;
-	public String address;
+	
+	Subject korea;
+	Subject math;
 	
 	//디폴트 생성자 - 사용자 지정 생성자가 존재하면 작동하지 않음
 
-	public Student(String name) { //사용자 지정 생성자
-		studentName = name;
-	}
-	
 	public Student(int id, String name) { //사용자 지정 생성자
 		studentID = id;
 		studentName = name;
-		address = "주소 없음";
+		korea = new Subject();
+		math = new Subject();
 	}
 	
-	public void showStudentInfo() {
-		System.out.println(studentName + ", " + address);
+	public void setKoreaSubject(String name, int score) {
+		korea.subjectName = name;
+		korea.score = score;
+	}
+	public void setMathSubject(String name, int score) {
+		math.subjectName = name;
+		math.score = score;
+	}
+	public void showStudentScore() {
+		int total = korea.score + math.score;
+		System.out.println(studentName + ": " + total);
 	}
 	
-	public String getStudentName() {
-		return studentName;
+	public static void main(String[] args) {
+		Student lee = new Student(100, "lee");
+		lee.setKoreaSubject("국어", 100);
+		lee.setMathSubject("수학", 95);
+		
+		lee.showStudentScore();
 	}
-	
-	
-	
 }

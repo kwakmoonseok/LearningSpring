b1db22670408697db78590f3d8980229fe1a0bfa
kwakmoonseok <kms34510@naver.com>
2020-12-24 14:05:50 +0900

    Day3 / 싱글톤 패턴 활용
    싱글톤 패턴을 활용한 확인 문제를 해결함

---
+++ JavaBasic/CardCompanyTest.java
@@ -0,0 +1,45 @@
+package JavaBasic;
+
+class CardCompany {
+	private static CardCompany instance = new CardCompany();
+	
+	private CardCompany() {
+		
+	}
+	
+	public static CardCompany getInstance() {
+		if (instance == null) {
+			instance = new CardCompany();
+		}
+		return instance;
+	}
+	
+	public Card createCard() {
+		Card card = new Card();
+		card.cardNum = ++Card.basicNum;
+		return card;
+	}
+}
+
+class Card {
+	static int basicNum = 10000;
+	int cardNum;
+	
+	public int getCardNumber() {
+		return cardNum;
+	}
+}
+public class CardCompanyTest {
+	public static void main(String[] args) {
+		CardCompany company = CardCompany.getInstance();
+	
+		Card myCard = company.createCard();
+		Card yourCard = company.createCard();
+		
+		System.out.println(myCard.getCardNumber());
+		System.out.println(yourCard.getCardNumber());
+	}
+	
+	
+	
+}

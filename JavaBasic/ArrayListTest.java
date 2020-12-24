b57243e51825433160b3964462b6c67215cabb37
kwakmoonseok <kms34510@naver.com>
2020-12-24 14:20:51 +0900

    Day3 / ArrayList
    배열과 비슷하지만 여러 메서드가 구현 되어있는 ArrayList를 사용했다.

---
+++ JavaBasic/ArrayListTest.java
@@ -0,0 +1,21 @@
+package JavaBasic;
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<>();
+		
+		list.add("I'm ");
+		list.add("NOT ");
+		list.add("Imposter.");
+		
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+		
+		for (String s: list) {
+			System.out.println(s);
+		}
+	}
+}
